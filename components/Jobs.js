0f3a086d41b2f8623cf621077c1fbe15b4d510df
bcflv-mario <103131627+bcflv-mario@users.noreply.github.com>
2022-07-06 20:43:31 +0200

    working users dashboard

---
+++ components/Jobs.js
@@ -0,0 +1,16 @@
+import Job from 'components/Job'
+
+
+const Jobs = ({ jobs, isDashboard }) => {
+  if (!jobs) return null
+
+  return (
+    <>
+      {jobs.map((job, index) => (
+        <Job key={index} job={job} isDashboard={isDashboard}/>
+      ))}
+    </>
+  )
+}
+
+export default Jobs
